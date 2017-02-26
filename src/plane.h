42753225548fa6d8357ef6c05fbdce34778cde31
9thScientist <bruno.ms.cancelinha@gmail.com>
2017-02-26 12:38:40 +0000

    Adds plane

---
+++ src/plane.h
@@ -0,0 +1,17 @@
+#ifndef __PLANE_H__
+#define __PLANE_H__
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+#include "vertex.h"
+
+/**
+ * Generates a square in the XZ plane, centered in the origin.
+ * @param size size of square
+ * @returns a vector with all the vertices generated for the plane
+ */
+std::vector<Vertex*> plane(float size);
+
+#endif
