7a72d7fa2e4580602886054cf321b60ccf28151b
zesilva63 <ze_dantas@hotmail.com>
2017-03-05 14:32:26 +0000

    last formula

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -6,42 +6,38 @@ std::vector<Vertex*> sphere(double raio, int camadasV, int camadasH) {
 
     std::vector<Vertex*> points;
 
-    float teta = 0; float fi = 0;
+    int i, j;
+    float teta = 0;
+    float fi = 0;
 	float saltoH = M_PI / camadasH;
 	float saltoV = 2 * (M_PI) / camadasV;
-	int i, j;
 	float x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4;
-
 	for (i = 0; i < camadasH; i++) {
 		teta = 0;
 
 		for (j = 0; j < camadasV; j++) {
+
 			x1 = raio*sin(fi)*sin(teta);
 			y1 = raio*cos(fi);
-			z1 = raio*sin(fi)*cos(teta);
+			z1 = raio*cos(teta)*sin(fi);
 
 			x2 = raio*sin(fi + saltoH)*sin(teta + saltoV);
 			y2 = raio*cos(fi + saltoH);
-			z2 = raio*sin(fi + saltoH)*cos(teta + saltoV);
+			z2 = raio*cos(teta + saltoV)*sin(fi + saltoH);
 
 			x3 = raio*sin(fi + saltoH)*sin(teta);
 			y3 = raio*cos(fi + saltoH);
-			z3 = raio*sin(fi + saltoH)* cos(teta);
+			z3 = raio*cos(teta)*sin(fi + saltoH);
 
-			x4 = raio*sin(fi)*sin(teta + saltoV);
-			y4 = raio*cos(fi);
-			z4 = raio*sin(fi)*cos(teta + saltoV);
-
+            points.push_back(new Vertex(x1, y1, z1));
+            points.push_back(new Vertex(x2, y2, z2));
+            points.push_back(new Vertex(x3, y3, z3));
 
 			printf("%f %f %f\n", x1, y1, z1);
 			printf("%f %f %f\n", x2, y2, z2);
 			printf("%f %f %f\n", x3, y3, z3);
 
-			printf("%f %f %f\n", x1, y1, z1);
-			printf("%f %f %f\n", x4, y4, z4);
-			printf("%f %f %f\n", x2, y2, z2);
-
-			teta += saltoV;
+            teta += saltoV;
 		}
 		fi += saltoH;
 	}
