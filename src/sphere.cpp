1dce95a805f7bf485d8f9f07360952ce63354af2
zesilva63 <ze_dantas@hotmail.com>
2017-03-05 15:32:30 +0000

    identation with tabs to spaces

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -13,38 +13,35 @@ std::vector<Vertex*> sphere(double radius, int verticalLayers, int horizontalLay
     std::vector<Vertex*> points;
     int i, j;
     float teta = 0, fi = 0;
-	float saltoH = PI / horizontalLayers;
-	float saltoV = (2 * PI) / verticalLayers;
-	float x1, y1, z1, x2, y2, z2, x3, y3, z3;
+    float jumpH = PI / horizontalLayers;
+    float jumpV = (2 * PI) / verticalLayers;
+    float x1, y1, z1, x2, y2, z2, x3, y3, z3;
 
     for (i = 0; i < horizontalLayers; i++) {
-		teta = 0;
+        teta = 0;
 
-		for (j = 0; j < verticalLayers; j++) {
+        for (j = 0; j < verticalLayers; j++) {
 
-			x1 = radius*sin(fi)*sin(teta);
-			y1 = radius*cos(fi);
-			z1 = radius*cos(teta)*sin(fi);
+            x1 = radius*sin(fi)*sin(teta);
+            y1 = radius*cos(fi);
+            z1 = radius*cos(teta)*sin(fi);
 
-			x2 = radius*sin(fi + saltoH)*sin(teta + saltoV);
-			y2 = radius*cos(fi + saltoH);
-			z2 = radius*cos(teta + saltoV)*sin(fi + saltoH);
+            x2 = radius*sin(fi + jumpH)*sin(teta + jumpV);
+            y2 = radius*cos(fi + jumpH);
+            z2 = radius*cos(teta + jumpV)*sin(fi + jumpH);
 
-			x3 = radius*sin(fi + saltoH)*sin(teta);
-			y3 = radius*cos(fi + saltoH);
-			z3 = radius*cos(teta)*sin(fi + saltoH);
+            x3 = radius*sin(fi + jumpH)*sin(teta);
+            y3 = radius*cos(fi + jumpH);
+            z3 = radius*cos(teta)*sin(fi + jumpH);
 
             points.push_back(new Vertex(x1, y1, z1));
             points.push_back(new Vertex(x2, y2, z2));
             points.push_back(new Vertex(x3, y3, z3));
 
-            printf("%f %f %f\n", x1, y1, z1);
-            printf("%f %f %f\n", x2, y2, z2);
-            printf("%f %f %f\n", x3, y3, z3);
-
-            teta += saltoV;
-		}
-		fi += saltoH;
+            teta += jumpV;
+        }
+        fi += jumpH;
 	}
+
     return points;
 }
